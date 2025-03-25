07c3b57217f716b6388006bf108e75123fc48310
doston9471 <doston.7194@gmail.com>
2025-03-25 23:00:10 +0100

    add calculateHash function

--- main.go
+++ main.go
@@ -25,6 +25,18 @@ type Block struct {
 	PrevHash  string
 }
 
+// Blockchain is a series of validated Blocks
+var Blockchain []Block
+
 func main(){
 	fmt.Println("Hello, World!")
+}
+
+// SHA256 hasing
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
 }
