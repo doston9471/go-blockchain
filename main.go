023db2009af5f6844a4a59a0cad0da65bbc911d5
doston9471 <doston.7194@gmail.com>
2025-03-25 22:55:56 +0100

    add main.go && import packages

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main(){
+	fmt.Println("Hello, World!")
+}
